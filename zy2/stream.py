96860aad013a794e5addf4b38aafde44b51eca86
3208247128 <3208247128@qq.com>
2024-04-27 11:31:36 +0800

    修改了一下主函数代码

---
+++ zy2/stream.py
@@ -0,0 +1,10 @@
+import redgreen
+import cv2
+
+# 检测红色和绿色区域并标记
+processed_image = redgreen.detect_red_and_green('zy2\image_2.jpeg')
+
+# 显示带有标记的图像
+cv2.imshow('Detected Colors', processed_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
